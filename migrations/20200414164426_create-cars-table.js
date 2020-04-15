9d8866ad3ef9a26c21854547202e4c715d49dd60
Asha <ashalbenjamin@gmail.com>
2020-04-14 21:25:23 -0500

    added notNullable and 128

--- migrations/20200414164426_create-cars-table.js
+++ migrations/20200414164426_create-cars-table.js
@@ -3,9 +3,9 @@ exports.up = function (knex, Promise) {
 	return knex.schema.createTable('cars', (tbl) => {
 		tbl.increments();
 		tbl.integer('vin', 128).unique().notNullable();
-		tbl.text('make');
-		tbl.text('model');
-		tbl.integer('mileage');
+		tbl.text('make', 128).notNullable();
+		tbl.text('model', 128).notNullable();
+		tbl.integer('mileage', 128).notNullable();
 		tbl.text('transmission');
 		tbl.text('title');
 	});
